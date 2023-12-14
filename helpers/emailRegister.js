94210ca68aaaebbb5f0274f6bfb3f0b5300e12d2
Leglocker01 <beristaindelamora@gmail.com>
2023-12-14 10:18:43 -0600

    done

--- helpers/emailRegister.js
+++ helpers/emailRegister.js
@@ -24,7 +24,7 @@ export const emailRegister = async (data) => {
     html: `<div style="background-color: #535b61; color: #fff; padding:10px;">
     <h1>Bienvenid@ ${name}, ¡Gracias por registrarte en PsychoTracker!</h1>
     <p>Para comenzar a utilizar nuestra plataforma, por favor confirma tu cuenta haciendo clic en el siguiente botón:</p> 
-    <a href="${process.env.FRONTEND_URL}/confirmation/${token}" style="background-color: #266FB3; color: #fff; padding:10px; text-decoration:none; border-radius:5px;">Confirmar Cuenta</a>. 
+    <a href="${process.env.FRONTEND_URL}/confirmation/${token}" style="background-color: #266FB3; color: #fff; padding:10px; text-decoration:none; border-radius:5px;">Confirmar Cuenta</a>
     <p>Una vez confirmada, podrás comenzar a realizar un seguimiento de tus pacientes y mejorar tu administración de manera efectiva y más facil. Recuerda que lo que se registra es más fácil de medir y mejorar en el tiempo.</p> <img src="https://storage.googleapis.com/pai-images/3ebd348edae84eccb20edd385ec840cc.jpeg" alt="Psychologist on therapy" width="270" height="290">
     </div>`,
   });
