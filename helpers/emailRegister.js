f035d592f6037e738ee0097d2b56465207570c53
Leglocker01 <beristaindelamora@gmail.com>
2024-01-05 08:20:13 -0600

    adding secure: true at node smtp

--- helpers/emailRegister.js
+++ helpers/emailRegister.js
@@ -8,6 +8,7 @@ export const emailRegister = async (data) => {
   const transport = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
